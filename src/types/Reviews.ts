6993d2f3f9aaab45da25e13b8cf1089aec3794d9
cdmvfx <cdm.vfx@gmail.com>
2022-12-25 01:04:14 -0500

    Type import fixes.

--- src/types/Reviews.ts
+++ src/types/Reviews.ts
@@ -1,19 +1,1 @@
 import type { Build, Review, User } from "@prisma/client";
-import type { BuildSerialized } from "./Builds";
-import type { UserSerialized } from "./Users";
-
-export type CompleteReviewData = Review & {
-	createdAt: string;
-	build?: Build
-	author: User;
-}
-
-export type ReviewSerialized = Omit<Review, "createdAt" | "updatedAt"> & {
-	createdAt: string;
-	updatedAt: string;
-}
-
-export type CompleteReviewDataSerialized = ReviewSerialized & {
-	build?: BuildSerialized
-	author: Pick<UserSerialized, "id" | "name" | "image">;
-}
